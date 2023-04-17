6574f63150ca758bf8da748a5628f534ebc8c5ca
lidor avisar <avisar0402@gmail.com>
2023-04-17 16:14:33 +0300

    e

--- routes/songs.js
+++ routes/songs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 bcrypt = require("bcrypt");
-const { SongModel } = require("../models/songModel")
+const { SongModel,validateSongs } = require("../models/songModel")
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -17,5 +17,38 @@ router.get("/", async (req, res) => {
         res.status(502).json({err})
     }
 })
+
+router.post('/', async (req, res) => {
+    const validBody = validateSongs(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details)
+    }
+    try {
+        const drink = new SongModel(req.body);
+        await drink.save();
+        res.status(201).json(drink);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const validBody = validateSongs(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details)
+    }
+    try {
+        const id = req.params.id;
+        const data = await SongModel.updateOne({ _id: id }, req.body);
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+
 
 module.exports = router;
